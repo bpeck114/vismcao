1a9d8b8cf682c39fefc93a954edf2e471e332538
Brianna Lynn Peck <bpeck@login35.chn.perlmutter.nersc.gov>
2024-11-25 13:39:12 -0800

    10lgs 8mag act study done

--- scripts/studies/act_count.py
+++ scripts/studies/act_count.py
@@ -16,7 +16,7 @@ args = parser.parse_args()
 
 # Set parameters for actuator study
 config_folder = "master_files/act_vs_lgs/10_lgs/8mag_lgs"
-default_output_folder = "studies/act_count/9_lgs/8mag_lgs/"
+default_output_folder = "studies/act_count/10_lgs/8mag_lgs/"
 test_folder = "studies/act_count/10_lgs/8mag_lgs/test/"
 act_count = np.array([1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500, 3750, 4000, 4250, 4500, 4750, 5000, 5250, 5500, 5750, 6000])
 
