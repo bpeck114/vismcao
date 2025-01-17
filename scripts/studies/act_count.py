369f2048458026c7b41c461828295b4740386e48
Brianna Peck <bpeck114@berkeley.edu>
2025-01-17 06:39:53 -0800

    Updated scripts for gradient study

--- scripts/studies/act_count.py
+++ scripts/studies/act_count.py
@@ -15,9 +15,9 @@ parser.add_argument("--test", action="store_true", help="Run in test mode withou
 args = parser.parse_args()
 
 # Set parameters for actuator study
-config_folder = "master_files/dm_orders/8_lgs/7mag_lgs"
-default_output_folder = "studies/act_count/8_lgs/7mag_lgs/"
-test_folder = "studies/act_count/8_lgs/7mag_lgs/test/"
+config_folder = "master_files/gradient/10_lgs/8mag_lgs"
+default_output_folder = "studies/gradient/10_lgs/8mag_lgs/"
+test_folder = "studies/gradient/10_lgs/8mag_lgs/test/"
 act_count = np.array([1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500, 3750, 4000, 4250, 4500, 4750, 5000, 5250, 5500, 5750, 6000])
 
 # Choose output folder based on test mode
